f209eb80177d9dfd8c2820f5758274d52f4b13c9
2g4423 <yoshifumi.tsuji.biz@gmail.com>
2022-01-24 20:41:40 +0900

    Add message on chocolate

--- js/main.js
+++ js/main.js
@@ -15,14 +15,14 @@ const spongeMaterial = new THREE.MeshLambertMaterial({ color: 0xf7e28b });
 // head
 const strawberries = new THREE.Group();
 for (let i = 0; i < 10; i++) {
-  const strawberry = new THREE.Mesh(new THREE.CylinderGeometry(0, 10, 10, 100), strawberryMaterial);
+  const strawberry = new THREE.Mesh(new THREE.CylinderGeometry(0, 10, 15, 100), strawberryMaterial);
   const radian = (i / 10) * Math.PI * 2;
-  strawberry.position.set(65 * Math.cos(radian), 35, 65 * Math.sin(radian));
+  strawberry.position.set(65 * Math.cos(radian), 32, 65 * Math.sin(radian));
   strawberries.add(strawberry);
 }
 
-const chocolate = new THREE.Mesh(new THREE.BoxGeometry(50, 2, 80), chocolateMaterial);
-chocolate.position.set(0, 30, 0);
+const chocolate = new THREE.Mesh(new THREE.BoxGeometry(100, 2, 50), chocolateMaterial);
+chocolate.position.set(0, 28, 0);
 
 // body
 const body = new THREE.Group();
@@ -47,6 +47,27 @@ const cake = new THREE.Group();
 cake.add(body, chocolate, strawberries);
 scene.add(cake);
 
+// text
+const loader = new THREE.FontLoader();
+loader.load(
+  'https://threejs-plactice.vercel.app/fontloader/fonts/helvetiker_regular.typeface.json',
+  function (font) {
+    const matLite = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        transparent: true,
+        opacity: 1,
+        side: THREE.DoubleSide
+      }), // マテリアルを設定
+      message = 'Happy birthday!!', // 出力する文字
+      shapes = font.generateShapes(message, 8), // 文字の大きさを設定
+      text_geometry = new THREE.ShapeGeometry(shapes), // ジオメトリを設定
+      text = new THREE.Mesh(text_geometry, matLite); // textという変数を作成し設定したジオメトリ・マテリアルにメッシュのクラスに引数として渡す
+    cake.add(text); // シーンに作成したtextを引数として渡す。
+    text.position.set(-40, 30, 5);
+    text.rotation.set(-1.54, 0, 0);
+  }
+);
+
 // light
 const light = new THREE.DirectionalLight(0xffffff, 0.9);
 light.position.set(0, 50, 30);
@@ -57,7 +78,8 @@ scene.add(ambient);
 
 // camera
 const camera = new THREE.PerspectiveCamera(90, width / height, 1, 1000);
-camera.position.set(60, 100, 140);
+const controls = new THREE.OrbitControls(camera, document.getElementById('stage'));
+camera.position.set(0, 120, 160);
 camera.lookAt(scene.position);
 
 // renderer
@@ -70,7 +92,7 @@ document.getElementById('stage').appendChild(renderer.domElement);
 function render() {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
-  cake.rotation.y += 0.01;
+  cake.rotation.y -= 0.01;
 }
 
 render();
