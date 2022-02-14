66cf04499ca9feab6996f273eb56ac5d54832115
2g4423 <yoshifumi.tsuji.biz@gmail.com>
2022-02-14 18:10:19 +0900

    For test

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@ canvas.height = height;
   );
 
   const msg = getUrlQueries().msg || 'Congratulations!!';
-  const canvasTexture = new THREE.CanvasTexture(createCanvasForTexture(width * 10, height * 10, msg, 28));
+  const canvasTexture = new THREE.CanvasTexture(createCanvasForTexture(width, height, msg, 28));
 
   const sprite = createSprite(
     canvasTexture,
