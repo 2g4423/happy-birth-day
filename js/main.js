01974b0e389daf37428179f3d772df3f6b95f020
2g4423 <yoshifumi.tsuji.biz@gmail.com>
2022-02-15 10:56:28 +0900

    For test

--- js/main.js
+++ js/main.js
@@ -77,14 +77,14 @@ canvas.height = height;
           transparent: true,
           opacity: 1,
           side: THREE.DoubleSide
-        }), // マテリアルを設定
-        message = 'Happy birthday!!', // 出力する文字
+        }),
+        message = 'Happy birthday!!',
         shapes = font.generateShapes(message, 8), // 文字の大きさを設定
         text_geometry = new THREE.ShapeGeometry(shapes), // ジオメトリを設定
         text = new THREE.Mesh(text_geometry, matLite); // textという変数を作成し設定したジオメトリ・マテリアルにメッシュのクラスに引数として渡す
-      cake.add(text); // シーンに作成したtextを引数として渡す。
-      text.position.set(-40, 30, 5);
-      text.rotation.set(-1.54, 0, 0);
+      chocolate.add(text); // シーンに作成したtextを引数として渡す。
+      text.position.set(-40, 1.1, 5);
+      text.rotation.set(-Math.PI / 2, 0, 0);
     }
   );
 
@@ -93,8 +93,8 @@ canvas.height = height;
 
   const sprite = createSprite(
     canvasTexture,
-    { x: width, y: width * (height / width), z: width },
-    { x: 0, y: 100, z: 0 }
+    { x: width, y: width * (height / width), z: 0 },
+    { x: 0, y: 0, z: 0 }
   );
 
   // light
@@ -144,7 +144,7 @@ function createCanvasForTexture(width, height, text, fontSize) {
   const ctx = canvasForText.getContext('2d');
   ctx.canvas.width = width;
   ctx.canvas.height = height;
-  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
   const fs = document.body.clientWidth < 1200 ? `${fontSize}vw` : `300px`;
